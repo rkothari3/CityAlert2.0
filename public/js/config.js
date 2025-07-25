9ba467e712814dae31705229ff3a8d4957b6eb73
Rk619504 <rajg6.isb@gmail.com>
2025-07-25 11:59:22 -0400

    Maps issue fixed (i hope)

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,7 @@
 // Configuration file for CityAlert frontend
 const API_BASE_URL = 'https://cityalert-backend.onrender.com/api';
+
+// Global configuration object
+window.CITY_ALERT_CONFIG = {
+    MAPS_API_KEY: window.GOOGLE_MAPS_API_KEY || 'YOUR_GOOGLE_MAPS_API_KEY_HERE'
+};
